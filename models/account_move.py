57c6a951a83e66853f26e553336259dcb34d9b76
NicolasZapata <35777697+NicolasZapata@users.noreply.github.com>
2024-05-20 11:51:24 -0500

    Invoice outstanding credit/debits widget.

--- models/account_move.py
+++ models/account_move.py
@@ -4,16 +4,22 @@ from odoo import _, api, fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    # TODO: Fix This binary field
+    prepayment_invoice_outstanding_credits_debits_widget = fields.Binary(
+        groups="account.group_account_invoice,account.group_account_readonly",
+        compute='_compute_payments_widget_to_reconcile_info',
+        exportable=False,
+    )
+ 
+
     def action_register_prepayment(self):
         """Open the account.payment.register wizard to pay the selected journal entries.
         :return: An action opening the account.payment.register wizard.
         """
-
         prepayment_journal_id = self.env["account.journal"].search(
             [
                 ("type", "in", ("bank", "cash", "crossed_prepayment")),
                 ("company_id", "=", self.company_id.id),
-                # ("id", "in", self.available_journal_ids.ids),
             ],
             limit=1,
         )
